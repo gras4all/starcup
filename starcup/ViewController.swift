5e3aabe0c54ed7dec3e38106ae906598cd36ff28
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2022-07-08 11:09:12 +0400

    Initial Commit

---
+++ starcup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starcup
+//
+//  Created by Андрей Груненков on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
